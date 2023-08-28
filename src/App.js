5b3a8d8397c497cc9ef51c03adbb3b9b394e8764
ozge <ozge.belenn@gmail.com>
2023-08-28 14:12:19 +0300

    data çekerken her istek atılımı sırasında diğerinin durdurulması gerçekleşti ve MovieDetails kısmında Escape tuşu ile çıkış ayarlandı

--- src/App.js
+++ src/App.js
@@ -98,6 +98,9 @@ export default function App() {
   };
 
   useEffect(() => {
+    //Seach alanından film arattığımızda çok hızlı bir şekilde giriş yaptığımız için veriler çakışır ve veriye ulaşamayız bunun için her istek attıldığında her harf için bir öncekini durdurmak gerekr. Bunun için React ile ilgisi olmayan browser ile ilgili olan AbortController fonksiyonunu kullanırız. ve Bunu datayı çektiğimiz yerden çağırıp en sonundada tekrar sıfırlıyoruz.
+    const controller = new AbortController();
+
     const fetchMovies = async () => {
       try {
         // Loding beklerken çalışması için
@@ -105,7 +108,8 @@ export default function App() {
         // Verileri almaya başlamadan önce her zaman erroru sıfırlıyoruzç
         setError("");
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+          `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+          { signal: controller.signal } //AbortControlerı bu şekilde obje olarak tanımlıyoruz.
         );
 
         // Burada eğer response başarılı dönmezse gösterilecek hata mevcut
@@ -115,10 +119,16 @@ export default function App() {
         // Eğer data false döndüyse özellikle loglayıp bu hatayı aldığımızda kendi yönümüze çevirmek için bunu uygulabiliriz.
         if (data.Response === "False") throw new Error("Movie not found");
         setMovies(data.Search);
-        console.log(data.Search);
+        // console.log(data.Search);
+        setError("");
       } catch (err) {
-        console.error(err.message);
+        // console.error(err.message);
         setError(err.message);
+
+        // AbrortErorü hata olarak yakaladığı için onu ayıklamak görmezden gelmek için
+        if (err.name !== "AbortError") {
+          setError(err.message);
+        }
       } finally {
         setIsLoding(false);
       }
@@ -341,8 +351,22 @@ const MovieDetails = ({ selectedId, onCloseMovie, onAddMovie, watched }) => {
     };
     onAddMovie(newMovieList);
     onCloseMovie();
-    console.log(newMovieList);
+    // console.log(newMovieList);
   };
+  // Escape tuşuya çıkış yapmak için
+  useEffect(() => {
+    const callback = (e) => {
+      if (e.code === "Escape") {
+        onCloseMovie();
+        // console.log("Closing");
+      }
+    };
+    document.addEventListener("keydown", callback);
+
+    return () => {
+      document.removeEventListener("keydown", callback);
+    };
+  }, [onCloseMovie]);
 
   useEffect(() => {
     const getMovieDetails = async () => {
@@ -359,10 +383,12 @@ const MovieDetails = ({ selectedId, onCloseMovie, onAddMovie, watched }) => {
   }, [selectedId]);
 
   useEffect(() => {
+    if (!title) return;
     document.title = `Movie | ${title}`;
 
     //useEffect içerisinde yan etkiyi sıfırlamak için temizleme fonksiyonu en başa dönüyor
     return () => (document.title = "usePopCorn");
+    // console.log(`Clean up effect for movie ${title}`);
   }, [title]);
 
   return (
