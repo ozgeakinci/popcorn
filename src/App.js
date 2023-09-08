5a6481d226bbfe0c37819c1742f72cd9aa9575ca
ozge <ozge.belenn@gmail.com>
2023-09-08 13:39:13 +0300

    useEffect ile dom öğesine ulaşmak

--- src/App.js
+++ src/App.js
@@ -1,65 +1,26 @@
 import { useEffect, useState } from "react";
 import StarRating from "./StarRating";
 
-const tempMovieData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt0133093",
-    Title: "The Matrix",
-    Year: "1999",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt6751668",
-    Title: "Parasite",
-    Year: "2019",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-  },
-];
-
-const tempWatchedData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-    runtime: 148,
-    imdbRating: 8.8,
-    userRating: 10,
-  },
-  {
-    imdbID: "tt0088763",
-    Title: "Back to the Future",
-    Year: "1985",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-    runtime: 116,
-    imdbRating: 8.5,
-    userRating: 9,
-  },
-];
 const KEY = "e0667f68";
 
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
 export default function App() {
-  const tempQuery = "godfather";
-  const [query, setQuery] = useState(tempQuery);
+  const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
+
   const [isLoding, setIsLoding] = useState(false);
   const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState(null);
+
+  // const [watched, setWatched] = useState([]);
+
+  const [watched, setWatched] = useState(() => {
+    const storeValue = localStorage.getItem("watched");
+    const initialWatched = storeValue ? JSON.parse(storeValue) : [];
+    return initialWatched;
+  });
 
   //useEffek kullanımı test
   // useEffect(() => {
@@ -91,13 +52,15 @@ export default function App() {
 
   const handleAddMovie = (movie) => {
     setWatched((movies) => [...movies, movie]);
-
-    localStorage.setItem("watched", JSON.stringify([...watched, movie]));
   };
 
   const handleDeleteWatch = (id) => {
     setWatched((movies) => movies.filter((movie) => movie.imdbID !== id));
   };
+
+  useEffect(() => {
+    localStorage.setItem("watched", JSON.stringify(watched));
+  }, [watched]);
 
   useEffect(() => {
     //Seach alanından film arattığımızda çok hızlı bir şekilde giriş yaptığımız için veriler çakışır ve veriye ulaşamayız bunun için her istek attıldığında her harf için bir öncekini durdurmak gerekr. Bunun için React ile ilgisi olmayan browser ile ilgili olan AbortController fonksiyonunu kullanırız. ve Bunu datayı çektiğimiz yerden çağırıp en sonundada tekrar sıfırlıyoruz.
@@ -232,6 +195,14 @@ const NumResult = ({ movies }) => {
   );
 };
 const Search = ({ query, setQuery }) => {
+  //Normalde bir dom öğresine ulaşmak için useEffect kullanarak aşağıdaki gibi ilerleyebiliriz.
+  //Ancak bunun yerine useRef kullanmak çok daha sağlıklıdır.
+  // useEffect(() => {
+  //   const el = document.querySelector(".search");
+  //   console.log(el);
+  //   el.focus();
+  // }, []);
+
   return (
     <input
       className="search"
