77f4f26601821b9f469d5f5b2a657253b3c29412
ozge <ozge.belenn@gmail.com>
2023-09-08 14:11:11 +0300

    useEffect ile search alını dinleyip entera basıldığında aktif olması ve içerisindeki verinin silinmesi

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import StarRating from "./StarRating";
 
 const KEY = "e0667f68";
@@ -203,6 +203,25 @@ const Search = ({ query, setQuery }) => {
   //   el.focus();
   // }, []);
 
+  const inputEl = useRef(null);
+
+  //ref ile sadece dom öğresine erişim sağlamı oluyoruz. Şimdi ona ulaşmak için useEffect kullanmalıyız.
+  // ilk render edildiğinde search alanına focus olması için
+  useEffect(() => {
+    //Bu fonksiyon seçtğimiz inputEl aktif oldığunda içindeki mevcut current elementi seçer.
+    //Başka bir noktadayken entera bastığımızda içindeki elemente focus olup silmesini sağlar
+    const callback = (e) => {
+      if (document.activeElement === inputEl.current) return;
+      if (e.code === "Enter") {
+        inputEl.current.focus();
+        setQuery("");
+      }
+    };
+
+    document.addEventListener("keydown", callback);
+    return () => document.addEventListener("keydown", callback);
+  }, [setQuery]);
+
   return (
     <input
       className="search"
@@ -210,6 +229,7 @@ const Search = ({ query, setQuery }) => {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 };
